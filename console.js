7e33da8e4dd6afdee95095f12b736ec565ac0a51
Cemil Can Almaz <65957701+cemilcan99@users.noreply.github.com>
2024-05-22 01:55:28 +0300

    Update console.js

--- console.js
+++ console.js
@@ -14,4 +14,25 @@
     </script>
 </body>
 </html>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form İşleme</title>
+</head>
+<body>
+    <form id="myForm">
+        Ad: <input type="text" id="name"><br>
+        <input type="submit" value="Gönder">
+    </form>
+
+    <script>
+        document.getElementById("myForm").addEventListener("submit", function(event) {
+            event.preventDefault();  // Formun gönderilmesini engelle
+            var name = document.getElementById("name").value;
+            console.log("Ad: " + name);
+        });
+    </script>
+</body>
+</html>
+
 
