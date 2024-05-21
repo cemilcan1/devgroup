939e18ea051dac0c4ec4fa8c00054d8801b471a9
Cemil Can Almaz <65957701+cemilcan99@users.noreply.github.com>
2024-05-22 01:54:07 +0300

    Update console.js

--- console.js
+++ console.js
@@ -1,14 +1,17 @@
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
-console.log("Hello")
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Buton Tıklaması</title>
+</head>
+<body>
+    <button onclick="showMessage()">Tıkla</button>
+    <p id="message"></p>
+
+    <script>
+        function showMessage() {
+            document.getElementById("message").innerHTML = "Butona tıkladınız!";
+        }
+    </script>
+</body>
+</html>
+
